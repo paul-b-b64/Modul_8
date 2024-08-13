e2cd2b9937bdb6e731d1e0797bb429aa80bb33c6
paul-b-b64 <belousko@inbox.ru>
2024-08-13 21:00:01 +0300

    ДЗ 8.1

---
+++ Modul_8_1.py
@@ -0,0 +1,18 @@
+def add_everything_up(a, b):
+    try:
+        result = a + b
+    except:
+        if isinstance(a, float or int):
+            a = str(a)
+        if isinstance(a, str):
+            b = str(b)
+        result = f'{a}{b}'
+    finally:
+        return result
+
+
+
+print(add_everything_up(123.456, 'строка'))
+print(add_everything_up('яблоко', 4215))
+print(add_everything_up(123.456, 7))
+print(add_everything_up('котло', 'надзор'))
