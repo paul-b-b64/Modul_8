df3360d45388924efc06fc7c24d95fcd0806f32f
paul-b-b64 <belousko@inbox.ru>
2024-08-16 14:46:07 +0300

    ДЗ 8.3

---
+++ Modul_8_3.py
@@ -0,0 +1,42 @@
+class Car:
+    def __init__(self, model, __vin, __numbers):
+        self.model = model
+        if self.__is_valid_vin(__vin):
+            self.__vin = __vin
+        if self.__is_valid_numbers(__numbers):
+            self.__numbers = __numbers
+
+    def __is_valid_vin(self, vin_number):
+        if not isinstance(vin_number, int):
+            raise IncorrectVinNumber('Некорректный тип VIN номера')
+        if vin_number not in range(1000000, 9999999):
+            raise IncorrectVinNumber('Неверный диапазон для vin номера')
+        return True
+
+    def __is_valid_numbers(self, numbers):
+        if not isinstance(numbers, str):
+            raise IncorrectCarNumbers('Некорректный тип данных для номеров')
+        if len(numbers) != 6:
+            raise IncorrectCarNumbers('Неверная длина номера')
+        return True
+
+class IncorrectVinNumber(Exception):
+    def __init__(self, message):
+        self.message = message
+
+class IncorrectCarNumbers(Exception):
+    def __init__(self, message):
+        self.message = message
+
+
+try:
+    first = Car('Model1', 1000080, 'f123hj')
+except IncorrectVinNumber as exc:
+    print(exc.message)
+except IncorrectCarNumbers as exc:
+    print(exc.message)
+else:
+    print(f'{first.model} успешно создан')
+
+
+
